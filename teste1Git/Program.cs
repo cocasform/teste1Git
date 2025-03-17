c6b64da2c77c42e13b7c8ca3a5f49d0697b8ffb5
SaraFormoso <saraformoso@gmail.com>
2025-03-17 18:29:56 +0000

    Update Program.cs
    adicionei a segunda linha de codigo

--- teste1Git/Program.cs
+++ teste1Git/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Esta é a minha primeira linha de codigo");
             Console.WriteLine("esta é a linha de codigo da branch testes");
+            Console.WriteLine("esta é a segunda linha de codigo");
         }
     }
 }
