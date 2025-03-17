0d8ccaacc92b26ac5b4f991e2af49f5d49608a25
SaraFormoso <saraformoso@gmail.com>
2025-03-17 18:40:31 +0000

    REverti
    reverti alterações no arquivo

--- teste1Git/Program.cs
+++ teste1Git/Program.cs
@@ -5,9 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
             Console.WriteLine("Esta é a minha primeira linha de codigo");
             Console.WriteLine("esta é a linha de codigo da branch testes");
             Console.WriteLine("esta é a segunda linha de codigo");
+
         }
     }
 }
