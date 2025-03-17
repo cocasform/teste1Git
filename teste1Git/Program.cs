67993f5c5f483555c1b3ac730c485d8841064cf5
SaraFormoso <saraformoso@gmail.com>
2025-03-17 16:09:50 +0000

    Criação de codigo na branch testes

--- teste1Git/Program.cs
+++ teste1Git/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Esta é a minha primeira linha de codigo");
+            Console.WriteLine("esta é a linha de codigo da branch testes");
         }
     }
 }
